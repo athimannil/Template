b5776e129e2a8e965e0885a671233e5db0a525ac
Muhammed Athimannil <hello@athimannil.com>
2018-03-07 22:41:52 +0100

    add router with bootstrap 4

--- client/components/App.js
+++ client/components/App.js
@@ -1,19 +1,28 @@
-import React from 'react';
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: 'Mo'
-    };
-  }
+import React, { Component } from "react";
+import { Route, Switch } from 'react-router-dom';
 
+import { Header } from './header/Header';
+import { Home } from './home/Home';
+import { Members } from './members/Members';
+import { About } from './about/About';
+
+class App extends React.Component {
   render() {
     return (
       <main className="main">
-        <h1>Hello mate !</h1>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={Home}/>
+          <Route path='/members' component={Members}/>
+          <Route path='/about' component={About}/>
+        </Switch>
       </main>
     );
   }
 }
+
+// App.propTypes = {
+//   children: propTypes.object.isRequired
+// }
 
 export default App;
